fe2e1f14fd358db9e274761e015a37e172838c16
pleasantvik <adedayo.ayoola5683@gmail.com>
2023-01-31 03:08:34 +0100

    added auth and user data from google

--- components/User.jsx
+++ components/User.jsx
@@ -1,5 +1,27 @@
-import React from "react";
+import React, { Fragment } from "react";
+import { signIn, signOut, useSession } from "next-auth/react";
 
 export const User = () => {
-  return <div>User</div>;
+  const { data: session } = useSession();
+
+  return (
+    <Fragment>
+      {session && (
+        <img
+          src={session.user.image}
+          onClick={() => signOut()}
+          className="h-10 w-10 rounded-full hover:bg-gray-200 cursor-pointer p-1"
+        />
+      )}
+
+      {!session && (
+        <button
+          onClick={signIn}
+          className="bg-blue-500 text-white px-6 py-2 font-medium rounded-md hover:brightness-105 hover:shadow-md "
+        >
+          Sign in
+        </button>
+      )}
+    </Fragment>
+  );
 };
---
+++ pages/auth/signin.jsx
@@ -0,0 +1,45 @@
+import { Header } from "@/components/Header";
+import React, { Fragment } from "react";
+import { getProviders, signIn } from "next-auth/react";
+import Image from "next/image";
+
+export default function Signin({ providers }) {
+  return (
+    <Fragment>
+      <Header />
+      <div className="mt-40">
+        {Object.values(providers).map((provider) => (
+          <div key={provider.name} className="flex flex-col items-center">
+            <Image
+              priority
+              src="/googl.webp"
+              // src="https://play-lh.googleusercontent.com/1-hPxafOxdYpYZEOKzNIkSP43HXCNftVJVttoo4ucl7rsMASXW3Xr6GlXURCubE1tA=w3840-h2160-rw"
+              height={144}
+              width={144}
+              alt="Google Logo"
+              className="w-52 object-cover"
+            />
+            <p className="text-sm italic my-10 text-center">
+              This website is created for learning purpose
+            </p>
+            <button
+              onClick={() => signIn(provider.id, { callbackUrl: "/" })}
+              className="bg-red-400 rounded-lg text-white p-3 hover:bg-red-500"
+            >
+              Sign in with {provider.name}
+            </button>
+          </div>
+        ))}
+      </div>
+    </Fragment>
+  );
+}
+
+// To get info from server
+export const getServerSideProps = async () => {
+  const providers = await getProviders();
+
+  return {
+    props: { providers },
+  };
+};
