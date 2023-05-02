1f6df2517bf5a7dbdb629d87a34dc724206866f8
pleasantvik <adedayo.ayoola5683@gmail.com>
2023-05-02 13:07:29 +0100

    about page

--- components/SearchResult.jsx
+++ components/SearchResult.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Parser from "html-react-parser";
 
 export default function SearchResult({ results }) {
   return (
@@ -7,6 +8,25 @@ export default function SearchResult({ results }) {
         About {results?.searchInformation?.formattedTotalResults} results{" "}
         {results?.searchInformation?.formattedSearchTime} seconds{" "}
       </p>
+
+      {results?.items.map((res) => (
+        <div key={res.link} className="max-w-xl mb-8">
+          <div className="group">
+            <a className="text-sm truncate" href={res.link}>
+              {res.formattedUrl}
+            </a>
+            <a
+              className="group-hover:underline decoration-blue-800"
+              href={res.link}
+            >
+              <h2 className="truncate text-xl font-medium text-blue-800">
+                {res.title}
+              </h2>
+            </a>
+          </div>
+          <p className="text-gray-600">{Parser(res.htmlSnippet)}</p>
+        </div>
+      ))}
     </div>
   );
 }
